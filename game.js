8692e8f6ee1095a019e53d7abc791a429001b81a
unknown <Mona-akbarpoor>
2019-04-05 19:34:58 +0300

    Image can move car

--- game.js
+++ game.js
@@ -1,3 +1,21 @@
-const car = document.getElementById('car')
+const car = document.getElementById('car');
+car.style.right = '100%';
 
-car.style.right = '200px'
+// first step: display 1, 2, ... 100
+// Second step: display 1%, 2%,..., 100%
+// Last step: set the right position from 1%, ..., to 100%
+
+
+for(let i = 0; i <= 90; i++) {
+  setTimeout(() => {
+    car.style.right = i + '%';
+  }, 200*i)
+}
+
+const car2 = document.getElementById('car2');
+function moveCar(position) {
+  if (position > 100) return;
+  car2.style.right = position + '%';
+  setTimeout(() => moveCar(position + 1), 200);
+}
+moveCar(0);
